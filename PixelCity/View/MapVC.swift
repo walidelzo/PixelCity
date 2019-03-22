6290a931aa83ff0cdcc183c507b2c364722ce551
Admin <walidelzo@gmail.com>
2019-03-22 09:27:50 +0200

    add my projet files

---
+++ PixelCity/View/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  MapVC.swift
+//  PixelCity
+//
+//  Created by Admin on 3/22/19.
+//  Copyright © 2019 NanoSoft. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
